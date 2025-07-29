de1089a6217c814f19ad4dff30485c4cf42a4851
Mimo <76134669+Ridesu@users.noreply.github.com>
2025-07-29 11:30:20 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=78y&os(cl91@f=7$jg$-p%m!!d)g3nl^lwo_*#&blj6z)#b%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'portfolio-siz1.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
 
 
 # Application definition
